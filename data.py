defce2c7b0edc4f88cc992d697bba43424ed8440
jsantos <nosliajalves@gmail.com>
2025-10-05 13:09:58 +0100

    commit

--- data.py
+++ data.py
@@ -1,1 +1,7 @@
-# Arquivo com constantes
+URBAN_ROUTES_URL = 'https://cnt-3ccf4fde-1e1a-48a6-8b87-8b64e20c6e2a.containerhub.tripleten-services.com?lng=pt'
+ADDRESS_FROM = 'East 2nd Street, 601'
+ADDRESS_TO = '1300 1st St'
+PHONE_NUMBER = '+1 123 123 12 12'
+CARD_NUMBER = '1234 5678 9100'
+CARD_CODE = '1111'
+MESSAGE_FOR_DRIVER = 'Pare no bar de sucos'
--- main.py
+++ main.py
@@ -1,2 +1,53 @@
+import data
+import helpers
+
 
 class TestUrbanRoutes:
+    @classmethod
+    def setup_class(cls):
+        if helpers.is_url_reachable(data.URBAN_ROUTES_URL):
+            print("Conectado ao servidor Urban Routes")
+        else:
+            print("Não foi possível conectar ao Urban Routes. Verifique se o servidor está ligado e ainda em execução")
+
+    def test_set_route(self):
+        # Adicionar em S8
+        print("Função criada para definir a rota")
+        pass
+
+    def test_select_plan(self):
+        # Adicionar em S8
+        print("Função criada para selecionar o plano")
+        pass
+
+    def test_fill_phone_number(self):
+        # Adicionar em S8
+        print("Função criada para preencher o número de telefone")
+        pass
+
+    def test_fill_card(self):
+        # Adicionar em S8
+        print("Função criada para preencher o número do cartão")
+        pass
+
+    def test_comment_for_driver(self):
+        # Adicionar em S8
+        print("Função criada para fazer um comentário")
+        pass
+
+    def test_order_blanket_and_handkerchiefs(self):
+        # Adicionar em S8
+        print("Função criada para pedir um cobertor e lenços")
+        pass
+
+    def test_order_2_ice_creams(self):
+        numbers_of_ice_creams = 2
+        for count in range(numbers_of_ice_creams):
+            # Adicionar em S8
+            print("Função criada para pedir sorvete")
+        pass
+
+    def test_car_search_model_appears(self):
+        # Adicionar em S8
+        print("Função criada para escolher o modelo do carro")
+        pass
